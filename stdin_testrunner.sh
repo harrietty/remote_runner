f174f139a2ebd1cee7b7484feb222f401878bddb
Harriet Ryder <harriethryder@gmail.com>
2018-11-23 13:29:05 +0000

    Save code and run tests from the entrypoint

--- stdin_testrunner.sh
+++ stdin_testrunner.sh
@@ -4,6 +4,21 @@ challenge_id=$1
 
 echo "Challenge ID is ${challenge_id}"
 
-while read line; do
-  echo $line
-done
+# Make a new file containing the user submitted code
+
+touch code.sh
+
+chmod +x code.sh
+
+cat > code.sh
+
+# Execute the tests
+
+bats --tap ./tests/${challenge_id}/tests.bats
+
+echo ${?}
+
+# Delete the original code
+
+rm ./code.sh
+
